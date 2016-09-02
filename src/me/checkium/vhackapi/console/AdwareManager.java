e5ac6ab31b765187173141006982e27aeaad2fad
angelbirth <ricky.ason@gmail.com>
2016-09-02 23:57:55 +0700

    Merge branch 'master' of https://github.com/vHack-API/vHackAPI-Java into experimental
    Conflicts:
    	src/me/checkium/vhackapi/console/AdwareManager.java
    	src/me/checkium/vhackapi/vHackAPI.java

--- src/me/checkium/vhackapi/console/AdwareManager.java
+++ src/me/checkium/vhackapi/console/AdwareManager.java
@@ -1,74 +1,68 @@
 package me.checkium.vhackapi.console;
 
+import me.checkium.vhackapi.Utils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import me.checkium.vhackapi.Utils;
-
 public class AdwareManager {
 
-	protected String password;
-	protected String username;
-	public AdwareManager(String pass, String user) {
-		password = pass;
-		username = user;
-	}
-	public boolean uploadAdware(String target) {
-		
-		URLConnection in;
-		try {
-			in = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + target, "vh_adwareUpload.php")).openConnection();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader((in.getInputStream())));
-		String line = br.readLine();
-		return (line.contains("0"));
-		}  catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public boolean removeAdware(String target) {
-		@SuppressWarnings("unused")
-		URLConnection in;
-		try {
-			in = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + target, "vh_removeAdwareRemote.php")).openConnection();
-			BufferedReader br = new BufferedReader(new InputStreamReader((in.getInputStream())));
-			String line = br.readLine();
-			JSONObject obj = new JSONObject(line);
-			return obj.getInt("result")==0;
-		} catch (IOException e) {
-			e.printStackTrace();
-		return false
-		}
-		
+    protected String password;
+    protected String username;
 
-		
-	}
-	
-	
-	public HashMap<String, Integer> getAdware() {
-		HashMap<String, Integer> map = new HashMap<>();
-		JSONObject json = Utils.JSONRequest("user::::pass", username + "::::" + password, "vh_adwareInfo.php");
-		if (json.getInt("remote") <= 0) {
-			return null;
-		} 
-			JSONArray array = json.getJSONArray("data");
-		    for (Object o : array) {
-			JSONObject obj= ((JSONObject) o);
-			String IP = obj.getString("ip");
-			int earn = obj.getInt("earn");
-			map.put(IP, earn);
-		}
-		return map;
-	}
+    public AdwareManager(String pass, String user) {
+        password = pass;
+        username = user;
+    }
+
+    public boolean uploadAdware(String target) {
+        URLConnection in;
+        try {
+            in = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + target, "vh_adwareUpload.php")).openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader((in.getInputStream())));
+            String line = br.readLine();
+            return (line.contains("0"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeAdware(String target) {
+        @SuppressWarnings("unused")
+        URLConnection in;
+        try {
+            in = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + target, "vh_removeAdwareRemote.php")).openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader((in.getInputStream())));
+            String line = br.readLine();
+            JSONObject obj = new JSONObject(line);
+            return obj.getInt("result") == 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public HashMap<String, Integer> getAdwares() {
+        HashMap<String, Integer> map = new HashMap<>();
+        JSONObject json = Utils.JSONRequest("user::::pass", username + "::::" + password, "vh_adwareInfo.php");
+        if (json.getInt("remote") <= 0) {
+            return null;
+        }
+        JSONArray array = json.getJSONArray("data");
+        for (Object o : array) {
+            JSONObject obj = ((JSONObject) o);
+            String IP = obj.getString("ip");
+            int earn = obj.getInt("earn");
+            map.put(IP, earn);
+        }
+        return map;
+    }
 }
